4618e3ef358722d327a57e6bb07e2daa9bc09158
jerocam <ocamposjero@gmail.com>
2021-04-07 21:42:51 -0700

    committed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import { LoaderProvider, useLoading, BallTriangle } from '@agney/react-loading';
 
 ReactDOM.render(
   <React.StrictMode>
